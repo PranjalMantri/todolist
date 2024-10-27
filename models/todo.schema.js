015fb797203a27e582afa8f92ee6e4d510d4f067
Pranjal Mantri <pranjalmantri@example.com>
2024-10-27 23:07:28 +0530

    Created User and Todo Database Models

---
+++ models/todo.schema.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false,
+  },
+});
---
+++ models/user.schema.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: password,
+    required: true,
+  },
+});
+
+export const User = new mongoose.models("User", userSchema);
